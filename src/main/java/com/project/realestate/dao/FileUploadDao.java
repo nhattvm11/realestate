cfbbeec08ff4c825542a34c2e3dd11f9fb9b2db9
Anh <anhphan96dn@gmail.com>
2018-04-11 09:43:44 +0700

    anh

--- src/main/java/com/project/realestate/dao/FileUploadDao.java
+++
@@ -1,9 +0,0 @@
-package com.project.realestate.dao;
-import java.util.List;
-import com.project.realestate.model.UploadedFile;
-
-public interface FileUploadDao {
-    List<UploadedFile> listFiles();
-    UploadedFile getFile(Long id);
-    UploadedFile saveFile(UploadedFile uploadedFile);
-}
--- src/main/java/com/project/realestate/service/FileUploadService.java
+++
@@ -1,9 +0,0 @@
-package com.project.realestate.service;
-import com.project.realestate.model.UploadedFile;
-import java.util.List;
-
-public interface FileUploadService {
-    List<UploadedFile> listFiles();
-    UploadedFile getFile(Long id);
-    UploadedFile saveFile(UploadedFile uploadedFile);
-}
